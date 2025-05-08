774480ed0ad022cd075b3f83bfec555d3483f7c4
NicolasRaf <nicolasalves136@gmail.com>
2025-05-08 17:01:24 -0300

    fix: remove interruption conflit and decress send time

--- Tarefa_Und.2-Com_IoT/Cloud/BoardCode/src/setup.c
+++ Tarefa_Und.2-Com_IoT/Cloud/BoardCode/src/setup.c
@@ -14,27 +14,4 @@ void initializeSystem() {
     initWifi(); // Inicializa o Wi-Fi
     initGpioButtonLeds(); // Inicializa os LEDs e bot es com GPIOs
     initHcsr04(); // Inicializa o sensor ultrass nico
-    initInterrupts(); // Configura as interrup es do sistema
 }
-
-void resetConnection() {
-    printf("Resetando conexão...\n");
-    blinkWarn();
-
-    sleep_ms(200);
-    close_tcp_connection();
-    retries = 0;
-    create_tcp_connection();
-    turnOffLeds();
-}
-
-/**
- * Configura as interrup es do sistema.
- *
- * Habilita interrupções para os botões e configura
- * as funções de callback para elas.
- */
-void initInterrupts() {
-    gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_RISE, true, resetConnection);
-}
-
